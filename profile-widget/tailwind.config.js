05e68a182b912d18dda077935f891d9a51add50b
Shruti <shrutishinde736@gmail.com>
2023-05-31 20:23:11 +0530

    profile widget added

---
+++ profile-widget/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        "neon-blue": "#5451fa",
+        lavender: "#dcdafe",
+        armadillo: "#4a4a4a",
+        aluminum: "#979797",
+      },
+      fontFamily: {
+        custom: "Roboto",
+      },
+    },
+  },
+  plugins: [],
+};
